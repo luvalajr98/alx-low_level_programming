bf4423fa9b16d695f5f1e4c8d3e69bcfdf144710
luvalajr98 <luvalajrcalvin98@gmail.com>
2022-09-08 16:25:44 -0700

    C program that prints exactly and that piece of art is useful

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,11 @@
 
 /**
  * main -Entry point of text
- * Return: Always 1 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
-
 {
-	write(2);and" that __attribute__(piece of art is useful\- Dora Korpar
-	2015-10-19);n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
