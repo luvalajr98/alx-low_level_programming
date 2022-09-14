5563bdb1e6eec6056cf7475deccbf288245ce4cd
Kelvin Luvala <47374118+luvalajr98@users.noreply.github.com>
2022-09-14 21:32:31 +0300

    outputs absolute value of integer input

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - outputs absolute value of integer input
+ * @n: integer to check
+ *
+ * Return: n
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+
+
+	else
+	{
+		n *= -1;
+		return (n);
+	}
+}
