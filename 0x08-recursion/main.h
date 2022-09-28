8fd2163e5b62de71471206326a18756f30948946
Kelvin Luvala <47374118+luvalajr98@users.noreply.github.com>
2022-09-28 18:34:31 +0300

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,15 +12,24 @@
 *
 */
 
+int _putchar(char c); /* Inputs characters*/
 
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int is_prime_number(int n);
-int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2);
+void _puts_recursion(char *s); /* Prints a string*/
+
+void _print_rev_recursion(char *s); /* Prints a string in reverse*/
+
+int _strlen_recursion(char *s); /* This function returns the length of a string*/
+
+int factorial(int n); /* This function returns the factorial of any given number*/
+
+int _pow_recursion(int x, int y); /* This function returns the value of x raised to power of y*/
+
+int _sqrt_recursion(int n); /* This function returns the natural square root of a number*/
+
+int is_prime_number(int n); /* This function returns 1 if the input integer is a prime number, otherwise return 0*/
+
+int is_palindrome(char *s); /* This function returns 1 if a string is a palindrome and 0 if not*/
+
+int wildcmp(char *s1, char *s2); /* This function compares two strings and returns 1 if the strings can be considered identical, otherwise return 0*/
 
 #endif /*MAIN_H*/
