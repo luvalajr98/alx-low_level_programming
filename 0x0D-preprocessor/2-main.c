f1ed292b935a175a1a1e6cb853ba9660c5c64d71
luvalajr98 <luvalajrcalvin98@gmail.com>
2022-10-12 09:56:16 -0700

    prints the name of file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - prints the name of the file it is compiled from
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
