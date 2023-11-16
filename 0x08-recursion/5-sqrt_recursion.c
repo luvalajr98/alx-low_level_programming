3cc61f614df855c3bc1be56ac159a7a587f330be
luvalajr98 <luvalajrcalvin98@gmail.com>
2023-11-16 03:49:12 -0800

    Fix: Finiding square roots of integers

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,17 +10,21 @@
  * Return: root square of the number
  *
  */
-int root_square(int num, int n)
+int root_square(int num, int n) //accepts and takes two integers as its parameters and returns an int as the result of the square root 
 {
-	/*Base case*/
+	//the base checks if the square of num equals to n, if yes, returns num as the square root of n 
 	if (num * num == n)
 	{
 		return (num);
 	}
+
+	//if the square of num exceeds n, function returns -1
 	else if (num * num > n)
 	{
 		return (-1);
 	}
+
+	//if not all base cases are satisfied, the recursive function is is recalled after increment of the num int
 	else
 	{
 		num++;
@@ -37,19 +41,25 @@ int root_square(int num, int n)
  */
 int _sqrt_recursion(int n)
 {
-	/* base case */
+	//base case to check if the int n is a negative
 	if (n < 0)
 	{
 		return (-1);
 	}
+
+	//checks if int n is 0, square root of 0 is 0
 	if (n == 0)
 	{
 		return (0);
 	}
+
+	//checks if int n = 1
 	if (n == 1)
 	{
 		return (1);
 	}
+
+	//if neither of the base cases is satisfied, a recursive function is called to find square root of n
 	else
 	{
 		return (root_square(2, n));
