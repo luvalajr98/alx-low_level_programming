70bd447550b0614dc3c7e457cf1b819bf1cccda7
luvalajr98 <luvalajrcalvin98@gmail.com>
2023-11-16 01:23:38 -0800

    Fix: String length with recursion!

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,10 +8,10 @@
 * Return: Returns an integer that is the length of the string
 */
 
-int _strlen_recursion(char *s)
+int _strlen_recursion(char *s) //function takes pointer to a string as its parameter
 {
-	if (*s != '\0')
-		return (1 + _strlen_recursion(s + 1));
+	if (*s != '\0') //checks if the current character is the null terminator
+		return (1 + _strlen_recursion(s + 1)); //inceremwnts with 1 to the next character untill it gets to the terminator, returning the length of string as an integer
 
-	return (0);
+	return (0); //terminates the recursive call once the teminator is reached and returns 0
 }
