81b0ad63bc7d0a37423718b5c5bcb434586ced7a
luvalajr98 <luvalajrcalvin98@gmail.com>
2023-11-16 02:48:48 -0800

    Fix: Working with power of integers

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,21 +8,22 @@
 * Return: value of x Power raised to y
 */
 
-int _pow_recursion(int x, int y)
+int _pow_recursion(int x, int y) //functioin that takes two integers and returns an integer representing x raised to power y
 {
 
-	if (y == 0)
+	if (y == 0) //checks if y is 0, if it is return 1, as any number raised to 0 is 1.
 
 	{
 		return (1);
 	}
-	if (y < 0)
+	if (y < 0) //checks if y is a negative number, return -1 if it is.
 	{
 		return (-1);
 	}
 	else
 	{
-		y--;
-		return (x * _pow_recursion(x, y));
+		y--; //if all parameter cases are satisfied, the program returns the results after a decrement of y until y = 0
+
+		return (x * _pow_recursion(x, y));//returns the value of x raised to the power of y
 	}
 }
