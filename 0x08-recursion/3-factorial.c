573eedfeda147eb1cb165a1f92bb72fb588c21c1
luvalajr98 <luvalajrcalvin98@gmail.com>
2023-11-16 02:02:33 -0800

    Fix: Factorial recursion!

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,14 +8,14 @@
 * Return: factorial number value
 */
 
-int factorial(int n)
+int factorial(int n) //takes an integer n as its parameter, returns an integer which represents the factorial of n
 {
 
-	if (n == 0)
+	if (n == 0) //checks whether the n = 0, if yes, returns 1
 	{
 		return (1);
 	}
-	if (n < 0)
+	if (n < 0) // checks if n is a negative value, if yes returns -1
 
 	{
 		return (-1);
@@ -23,6 +23,6 @@ int factorial(int n)
 
 	else
 	{
-		return (n * factorial(n - 1));
+		return (n * factorial(n - 1)); // only if n is a positive number other than 0, will repeat the recursive call untill n is 0
 	}
 }
