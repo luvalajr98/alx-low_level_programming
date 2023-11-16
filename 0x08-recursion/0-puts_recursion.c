c80fd830ec6c00121adc7c5d53e582f6ddee5c09
luvalajr98 <luvalajrcalvin98@gmail.com>
2023-11-16 00:56:08 -0800

    Fix: puts recursion program

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,16 +7,17 @@
 * Return: Always void.
 */
 
+// takes pointer to a character as its value
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s != '\0') //checks if the current character pointed to is not the null terminator that marks ed of string
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar(*s); //prints the current character
+		_puts_recursion(s + 1); //recursion is used with pointer s incrememnted by 1, towards the next character
 	}
 	else
 	{
-		_putchar('\n');
+		_putchar('\n'); //if the character pointed to is a null terminator, the function will print a new line and terminate the string
 	}
 }
 
