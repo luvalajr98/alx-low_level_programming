b9a6becc646f0c1b804d21830b62325970357f0e
luvalajr98 <luvalajrcalvin98@gmail.com>
2023-12-08 22:16:59 +0300

    Fix: Update on the print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * print_list - prints all the elements of a list_t list.
@@ -10,9 +11,9 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t increament;
+	size_t count;
 
-	increament = 0;
+	count = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -20,7 +21,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		increament++;
+		count++;
 	}
-	return (increament);
+	return (count);
 }
