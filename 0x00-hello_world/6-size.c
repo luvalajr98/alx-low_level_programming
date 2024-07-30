7b1c1fddd3609c7a70ccf885a226a29f7938dcf5
luvalajr98 <luvalajrcalvin98@gmail.com>
2024-07-30 15:46:41 +0300

    prints the sizes of characters

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,16 +2,16 @@
 
 /**
  * main - Prints size of characters
- *
+ * this program is mainly used to print the size of characters
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of an int: %zu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char)); //size of a single character
+	printf("Size of an int: %zu byte(s)\n", sizeof(int)); //size of an interger
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int)); //size of a long interger
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int)); //size of a long long interger
+	printf("Size of a float: %zu byte(s)\n", sizeof(float)); //size of a float
 	return (0);
 }
